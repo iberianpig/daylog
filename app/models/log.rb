abde593a47b50a7474afbc69f246d310cd3eb981
iberianpig <yhkyky@gmail.com>
2014-07-17 13:26:48 +0900

    fix validation and fix root of Routes

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,5 +1,5 @@
 class Log < ActiveRecord::Base
   validates :log_day, presence: true, uniqueness: true;
-  validates :motibation, presence: true, numericality: { only_integer: true, greater_than: 1, less_than: 5 };
+  validates :motibation, presence: true, numericality: { only_integer: true, greater_than: 0, less_than: 6 };
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Daylog::Application.routes.draw do
   resources :logs
+  root 'logs#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
